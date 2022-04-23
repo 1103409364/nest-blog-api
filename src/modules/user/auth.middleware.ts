f73cfa4462f7fd491343c539a51df786e6daaaef
1103409364 <1103409364@qq.com>
2022-04-23 14:58:30 +0800

    fix:  error message
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/modules/user/auth.middleware.ts
+++ src/modules/user/auth.middleware.ts
@@ -18,12 +18,12 @@ export class AuthMiddleware implements NestMiddleware {
     if (authHeaders && (authHeaders as string).split(' ')[1]) {
       const token = (authHeaders as string).split(' ')[1];
       const decoded = jwt.verify(token, SECRET);
-      const user = await this.userService.findById(decoded.id);
+      const { user } = await this.userService.findById(decoded.id);
 
       if (!user) {
         throw new HttpException('User not found.', HttpStatus.UNAUTHORIZED);
       }
-      req.user = user.user;
+      req.user = user;
       next();
     } else {
       throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
--- src/utils/error.filter.ts
+++ src/utils/error.filter.ts
@@ -13,6 +13,7 @@ interface ErrorResponse {
   path: string;
   message: string;
   stack?: string;
+  errors?: Record<string, string>;
 }
 
 @Catch() //装饰器传 HttpException 只处理 http 异常，不传可以处理所有异常
@@ -21,6 +22,7 @@ export class ErrorFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+
     const statusCode =
       error instanceof HttpException
         ? error.getStatus()
@@ -31,6 +33,8 @@ export class ErrorFilter implements ExceptionFilter {
       path: request.url,
       message: error.message,
     };
+    const errResponse = (error as any)?.response;
+    errorResponse.errors = errResponse?.errors || errResponse?._errors;
     // 未知异常返回 stack
     if (statusCode === HttpStatus.INTERNAL_SERVER_ERROR) {
       errorResponse.stack = error.stack;
