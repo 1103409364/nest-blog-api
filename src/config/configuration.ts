b0808da6d6ab6306d3ab693dd41da8ce84d678e4
1103409364 <1103409364@qq.com>
2023-05-05 17:11:12 +0800

    doc: socketPath comment
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -8,7 +8,7 @@ export const configuration = () => ({
     host: process.env.DATABASE_HOST || "localhost",
     port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
     type: process.env.DATABASE_TYPE || "mysql",
-    // socketPath: "/tmp/mysql.sock", // 配置 socketPath Linux 连不上
+    // socketPath: "/tmp/mysql.sock", // socketPath 是用于指定连接 MySQL 数据库时使用的 Unix 套接字文件的路径。 MySQL 数据库不是运行在与应用程序相同的计算机上，那么使用 socketPath 选项将会导致连接失败。
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DATABASE,
