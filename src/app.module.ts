f92593e7435cebb0e71fb7e1421f421e24dbc5c0
wcs <1103409364@qq.com>
2023-06-04 18:04:53 +0800

    chore: update orm config
    Signed-off-by: wcs <1103409364@qq.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,30 @@
 import { Logger, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-// import { join } from "path";
 import { UserModule } from "./modules/user/user.module";
 import { ArticleModule } from "./modules/article/article.module";
 import { ProfileModule } from "./modules/profile/profile.module";
 import { TagModule } from "./modules/tag/tag.module";
-import { configuration } from "./config/configuration";
+import ormConfig from "./config/orm.config";
 import { FileModule } from "./modules/file/file.module";
 import { StatusMonitorModule } from "nestjs-status-monitor";
 import { statusMonitorOption } from "./config/statusMonitor";
 import { serveStaticOption } from "./config/serveStatic";
-// import { ScheduleModule } from "@nestjs/schedule";
 // import { TasksModule } from "./modules/tasks/task.module";
 import { OcrModule } from "./modules/ocr/ocr.module";
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
-      load: [configuration],
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => config.get("database"),
-      inject: [ConfigService],
+      useFactory: ormConfig,
     }),
     ServeStaticModule.forRoot(serveStaticOption),
     StatusMonitorModule.forRoot(statusMonitorOption),
