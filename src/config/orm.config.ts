559d7c80c102c976e3cbf88ff92f38d0d315beed
wcs <1103409364@qq.com>
2023-06-18 15:56:09 +0800

    config: orm
    Signed-off-by: wcs <1103409364@qq.com>

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,4 +1,4 @@
-import { join } from "path";
+// import { join } from "path";
 import { registerAs } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
@@ -13,8 +13,8 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME, // 数据库名称
-    entities: [join(__dirname, "../", "**/**.entity{.ts,.js}")], // 实体类
-    autoLoadEntities: true, // 自动加载实体类
+    // entities: [join(__dirname, "../", "**/**.entity{.ts,.js}")], // 实体类
+    autoLoadEntities: true, // 自动加载实体类 与上面的 entities 二选一
     synchronize: process.env.DB_SYNC === "1", // 是否自动同步数据库表结构 自动建表、更新表字段等，生产环境不要使用
     logging: false, //process.env.NODE_ENV === 'dev', // 是否打印日志
     dropSchema: !!+process.env.DB_DROP_SCHEMA, // 每次初始化数据源时删除模式。谨慎使用此选项，不要在生产中使用它 - 否则您将丢失所有生产数据。此选项在调试和开发期间很有用。
