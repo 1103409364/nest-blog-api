6cb82811df76339df51846d6d17362d08cfd6ba2
wcs <1103409364@qq.com>
2024-05-30 20:04:16 +0800

    doc: swagger 请求参数示例
    Signed-off-by: wcs <1103409364@qq.com>

--- src/modules/translate/dto/trans.dto.ts
+++ src/modules/translate/dto/trans.dto.ts
@@ -1,9 +1,12 @@
 import { IsNotEmpty } from "class-validator";
 import { TranslateOption } from "../translate.interface";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class TransDto {
   @IsNotEmpty()
+  @ApiProperty({ example: "测试翻译" })
   text: string;
 
+  @ApiProperty({ example: '{"from": "zh-cn", "to": "en"}' })
   options: TranslateOption;
 }
